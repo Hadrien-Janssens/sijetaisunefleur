5f1df08305a1ca799d1347862cc4a9e6c89074b3
Hadrien Janssens <hadrien.janssens7@gmail.com>
2025-03-11 10:06:31 +0100

    minor change test

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+//minor changes test
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,5 +11,5 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
